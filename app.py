e43cd94991ca1ab663b44cfa50feece25fd1579d
oharatakaki <ohara.t@baseconnect.in>
2021-09-04 23:02:04 +0900

    複数選択＆変更

--- app.py
+++ app.py
@@ -1,76 +1,152 @@
-from matplotlib.backends.backend_pdf import PdfPages
 from io import BytesIO
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 import matplotlib.pyplot as plt
-from datetime import datetime, date
 import re
-from flask import Flask, render_template, request, redirect, flash, send_file, make_response, send_from_directory
+from flask import Flask, render_template, request, redirect, make_response, flash
 from flask.helpers import make_response
-from werkzeug.utils import secure_filename
 import pandas as pd
 import matplotlib
+import urllib.parse
+import json
 matplotlib.use('Agg')
 
 app = Flask(__name__)
 
 
-def transform(txt_data):
-    pulify1 = re.split("\r\n\r\n", txt_data.split(
-        'R.Time (min),Intensity')[1])[0]
-    pulify2 = re.split("\r\n\r\n", txt_data.split(
-        'R.Time (min),Intensity')[2])[0]
-    da1 = []
-    da2 = []
+def save_json(file_list):
+    save_array_data = []
+    for file in file_list:
+        file_name = file["file_name"]
+        txt_data = file["data"]
+        each_array_data = {"file_name": file_name, "content": []}
+        channel_list = txt_data.split("[LC Chromatogram(")
+        del channel_list[0]
+        for channel in channel_list:
+            name = re.search(r'(AD1|A-Ch2|A-Ch1)', channel).group(0)
+            raw_data = channel.split("R.Time (min),Intensity\n")[
+                1].split("\n\n")[0]
+            data = []
+            for row in raw_data.splitlines():
+                if row:
+                    data.append([row.split(",")[0], row.split(",")[1]])
+            channel_data = {"name": name, "data": data}
+            each_array_data["content"].append(channel_data)
+        save_array_data.append(each_array_data)
+    with open("data/data.json", "w") as f:
+        json.dump(save_array_data, f, indent=2)
 
-    for row in pulify1.splitlines():
-        if row:
-            da1.append([row.split(",")[0], row.split(",")[1]])
-    for row in pulify2.splitlines():
-        if row:
-            da2.append([row.split(",")[0], row.split(",")[1]])
 
-    df1 = pd.DataFrame(data=da1, columns=['Time', 'Intensity']).astype(float)
-    df2 = pd.DataFrame(data=da2, columns=['Time', 'Intensity']).astype(float)
-    data = [df1, df2]
-    time, a, b = 35, -10000, 4000000
-    fig = plt.figure()
-    t1 = data[0].iloc[:, 0]
-    t2 = data[1].iloc[:, 0]
-    y1 = data[0].iloc[:, 1]
-    y2 = data[1].iloc[:, 1]
+class Note:
+    def __init__(self, data):
+        self.fig = plt.figure()
+        self.data = data
 
-    ax = fig.add_subplot(3, 1, 1)
-    ax.plot(t1, y1, color="black", linewidth=0.8)
-    ax.plot(t2, y2, color="gray", linewidth=0.8)
-    ax.set_ylim([a, b])
-    ax.set_xlim([0, time])
-    fig.tight_layout()
-    fig.savefig("test.pdf")
+    def mix(self, num, time, a, b):
+        t1 = self.data[num-1]["data"][0].iloc[:, 0]
+        t2 = self.data[num-1]["data"][1].iloc[:, 0]
+        y1 = self.data[num-1]["data"][0].iloc[:, 1]
+        y2 = self.data[num-1]["data"][1].iloc[:, 1]
 
-    response = make_response(open("test.pdf", "rb").read())
+        ax = self.fig.add_subplot(3, 1, num)
+        ax.plot(t1, y1, color="black", linewidth=0.8)
+        ax.plot(t2, y2, color="gray", linewidth=0.8)
+        ax.set_ylim([a, b])
+        ax.set_xlim([0, time])
+        ax.set_title(self.data[num-1]["title"])
+        self.fig.tight_layout()
+
+
+def drow_graph(param):
+    with open("data/data.json", "r") as f:
+        all_data = json.load(f)
+    df_data_list = []
+    for each_data in all_data:
+        title = each_data["file_name"]
+        df1 = pd.DataFrame(data=each_data["content"][0]["data"], columns=[
+                           'Time', 'Intensity']).astype(float)
+        df2 = pd.DataFrame(data=each_data["content"][1]["data"], columns=[
+                           'Time', 'Intensity']).astype(float)
+        data = {"title": title, "data": [df1, df2]}
+        time = int(df1.iloc[-1]["Time"])
+        a = int(min(df1["Intensity"].min(), df2["Intensity"].min()))
+        b = int(max(df1["Intensity"].max(), df2["Intensity"].max()))
+        param.append({"time": time, "a": a, "b": b})
+        # 最初はparamが[]であるため必要な分がきちんと追加される
+        # 2回目以降は4つめ以降に追加されるから参照されなくなる
+        df_data_list.append(data)
+    note = Note(df_data_list)
+    for i in range(len(all_data)):
+        note.mix(i+1, param[i]["time"], param[i]["a"], param[i]["b"])
+
+    note.fig.savefig("data/test.pdf")
+    canvas = FigureCanvasAgg(note.fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    data = png_output.getvalue()
+
+    img_data = urllib.parse.quote(data)
+    return render_template('graph.html', img_data=img_data, param=param, size=len(all_data))
+
+
+def save_pdf():
+    response = make_response(open("data/test.pdf", "rb").read())
     response.headers['Content-Disposition'] = 'attachment; filename=test.pdf'
     response.mimetype = "application/pdf"
     return response
 
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+def save_txt(data_file):
+    with open("data/data_file.txt", mode='w') as f:
+        f.write(data_file.read().decode("shift_jis"))
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/transform', methods=["POST"])
-def transform_view():
-    app.logger.debug("________________________________________")
-    request_file = request.files["data_file"]
-    if not request_file:
-        return "NO FILE"
+@app.route('/help')
+def help():
+    return render_template('help.html')
 
-    file_contents = request_file.read().decode('shift_jis')
-    response = transform(file_contents)
+
+@app.route('/save', methods=["POST"])
+def save():
+    return save_pdf()
+
+
+@app.route('/drowgraph', methods=["POST"])
+def drowgraph_view():
+    request_file_list = request.files.getlist("data_file")
+
+    if len(request_file_list) > 0:
+        # 最初の読み込み時
+        if len(request_file_list) > 3:
+            return render_template("index.html", message="一度に読み込めるファイルは3つまでです")
+        elif request_file_list[0].filename == "":
+            return render_template("index.html", message="ファイルを選択してください")
+        elif len(request_file_list) > 0:
+            file_content_list = []
+            for file in request_file_list:
+                # 一旦1ファイル分だけ保存（直接開くと改行とかめんどい）
+                save_txt(file)
+                with open("data/data_file.txt", mode="r") as r:
+                    file_content = r.read()
+                # 1ファイル分だけ読み込んで変数に格納
+                file_content_list.append(
+                    {"data": file_content, "file_name": file.filename.split(".")[0]})
+            save_json(file_content_list)
+
+            param = []
+    else:
+        # 2回目以降
+        param = []
+        for i in range(int(request.form.get("size"))):
+            time = int(request.form.get("time"+str(i)))
+            a = int(request.form.get("a"+str(i)))
+            b = int(request.form.get("b"+str(i)))
+            param.append({"time": time, "a": a, "b": b})
+    response = drow_graph(param)
 
     return response
 
